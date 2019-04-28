4e276a250a615e72c57f7e409030f17bdf8c0847
Aly Norrgard <amnorrgard@gmail.com>
2019-04-28 15:06:00 -0400

    gitignored secrets by running git rm --cashed on the file

--- secrets.js
+++
@@ -1,6 +0,0 @@
-export const API_KEY = 'AIzaSyC9OJXIcIrcJBDPUfqkQ2WQTZrxHSssu5k';
-export const AUTH_DOMAIN = 'showmance-67892.firebaseapp.com';
-export const DB_URL = 'https://showmance-67892.firebaseio.com';
-export const PROJECT_ID = 'showmance-67892';
-export const STORAGE_BUCKET = 'showmance-67892.appspot.com';
-export const MSG_SENDER_ID = '293197621886';
