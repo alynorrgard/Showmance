15a83e2cfc4ca4b8acd08c64d56a31921d908226
Aly Norrgard <amnorrgard@gmail.com>
2019-04-28 15:02:40 -0400

    modified secrets file and gitignored it

--- secrets.js
+++ secrets.js
@@ -1,6 +1,6 @@
-export const API_KEY = 'AIzaSyBaCjBYEz8uMFm7O6pZn1DSsFAWYJluUXM';
-export const AUTH_DOMAIN = 'https://showmance-8da71.firebaseio.com';
-export const DB_URL = 'https://showmance-65397.firebaseio.com';
-export const PROJECT_ID = 'showmance-8da71';
-export const STORAGE_BUCKET = 'showmance-8da71.appspot.com';
-export const MSG_SENDER_ID = '626980894697';
+export const API_KEY = 'AIzaSyC9OJXIcIrcJBDPUfqkQ2WQTZrxHSssu5k';
+export const AUTH_DOMAIN = 'showmance-67892.firebaseapp.com';
+export const DB_URL = 'https://showmance-67892.firebaseio.com';
+export const PROJECT_ID = 'showmance-67892';
+export const STORAGE_BUCKET = 'showmance-67892.appspot.com';
+export const MSG_SENDER_ID = '293197621886';
